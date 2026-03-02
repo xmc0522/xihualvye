1f206ce55e9a2366be576964b9256f442e395c00
青衫憶笙 <16009524+xmc990408@user.noreply.gitee.com>
2026-03-02 11:26:32 +0800

    创建表格

--- xihualvye/src/App.vue
+++ xihualvye/src/App.vue
@@ -1,85 +1,176 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import './css/table.css'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div class="table-container">
+    <table class="data-table">
+      <!-- 标题行 -->
+      <tr>
+        <td colspan="6" class="table-title">天枢款-圆弧</td>
+      </tr>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <!-- 客户/日期信息行 -->
+      <tr class="info-row">
+        <td class="info-label">客户：</td>
+        <td colspan="2" class="info-value">中山乐事水族</td>
+        <td class="info-label">日期：</td>
+        <td colspan="2" class="info-value">2026/3/1</td>
+      </tr>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+      <!-- 表面/数量/规格信息行 -->
+      <tr class="info-row">
+        <td class="info-label">表面：</td>
+        <td class="info-value">冰川白</td>
+        <td class="info-label">数量：</td>
+        <td class="info-value">3套</td>
+        <td class="info-label">规格：</td>
+        <td class="info-value">L-1545DY</td>
+      </tr>
 
-  <RouterView />
+      <!-- 长度/宽度/高度信息行 -->
+      <tr class="info-row">
+        <td class="info-label">长度：</td>
+        <td class="text-red">1518</td>
+        <td class="info-label">宽度：</td>
+        <td class="text-red">609</td>
+        <td class="info-label">高度：</td>
+        <td class="text-red">883</td>
+      </tr>
+
+      <!-- 表头行 -->
+      <tr class="header-row">
+        <td>型号</td>
+        <td>图片</td>
+        <td>名称</td>
+        <td>规格</td>
+        <td>数量</td>
+        <td>备注</td>
+      </tr>
+
+      <!-- H-1055 新下包边 第一行 -->
+      <tr class="data-row">
+        <td class="col-model merged-cell" rowspan="2">H-1055</td>
+        <td class="col-image merged-cell" rowspan="2"></td>
+        <td class="col-name merged-cell" rowspan="2">新下包边</td>
+        <td>1434</td>
+        <td>6</td>
+        <td class="col-remark" rowspan="2">打磨喷漆</td>
+      </tr>
+      <!-- H-1055 新下包边 第二行 -->
+      <tr class="data-row">
+        <td>525</td>
+        <td>6</td>
+      </tr>
+
+      <!-- H-1073 立柱 -->
+      <tr class="data-row">
+        <td class="col-model">H-1073</td>
+        <td class="col-image"></td>
+        <td class="col-name">立柱</td>
+        <td>880</td>
+        <td>12</td>
+        <td class="col-remark"></td>
+      </tr>
+
+      <!-- H-1074 前后横梁 -->
+      <tr class="data-row">
+        <td class="col-model">H-1074</td>
+        <td class="col-image"></td>
+        <td class="col-name">前后横梁</td>
+        <td>1438</td>
+        <td>12</td>
+        <td class="col-remark"></td>
+      </tr>
+
+      <!-- H-1075 侧横梁 -->
+      <tr class="data-row">
+        <td class="col-model">H-1075</td>
+        <td class="col-image"></td>
+        <td class="col-name">侧横梁</td>
+        <td>469</td>
+        <td>12</td>
+        <td class="col-remark merged-cell" rowspan="2">冲孔</td>
+      </tr>
+
+      <!-- H-1076 中柱 -->
+      <tr class="data-row">
+        <td class="col-model">H-1076</td>
+        <td class="col-image"></td>
+        <td class="col-name">中柱</td>
+        <td>793</td>
+        <td>12</td>
+      </tr>
+
+      <!-- H-1005 拉筋 -->
+      <tr class="data-row">
+        <td class="col-model">H-1005</td>
+        <td class="col-image"></td>
+        <td class="col-name">拉筋</td>
+        <td>507</td>
+        <td>18</td>
+        <td class="col-remark"></td>
+      </tr>
+
+      <!-- H-1077 门料 第一行 -->
+      <tr class="data-row">
+        <td class="col-model merged-cell" rowspan="2">H-1077</td>
+        <td class="col-image merged-cell" rowspan="2"></td>
+        <td class="col-name merged-cell" rowspan="2">门料</td>
+        <td>476.7</td>
+        <td>18</td>
+        <td class="col-remark highlight-yellow">45度切</td>
+      </tr>
+      <!-- H-1077 门料 第二行 -->
+      <tr class="data-row">
+        <td>858</td>
+        <td>18</td>
+        <td class="col-remark highlight-yellow">一半冲孔</td>
+      </tr>
+
+      <!-- H-1077 侧板 第一行 -->
+      <tr class="data-row">
+        <td class="col-model merged-cell" rowspan="2">H-1077</td>
+        <td class="col-image merged-cell" rowspan="2"></td>
+        <td class="col-name merged-cell" rowspan="2">侧板</td>
+        <td>524</td>
+        <td>12</td>
+        <td class="col-remark merged-cell" rowspan="2" style="background-color: #ffff00;">45度切</td>
+      </tr>
+      <!-- H-1077 侧板 第二行 -->
+      <tr class="data-row">
+        <td>882</td>
+        <td>12</td>
+      </tr>
+
+      <!-- 门板（红色文字行） -->
+      <tr class="data-row red-row">
+        <td></td>
+        <td class="text-red">门板</td>
+        <td class="text-red">473.9</td>
+        <td class="text-red">855.2</td>
+        <td>9</td>
+        <td></td>
+      </tr>
+
+      <!-- 侧门板（红色文字行） -->
+      <tr class="data-row red-row">
+        <td></td>
+        <td class="text-red">侧门板</td>
+        <td class="text-red">521.2</td>
+        <td class="text-red">879.2</td>
+        <td>6</td>
+        <td></td>
+      </tr>
+
+      <!-- 配件表 -->
+      <tr>
+        <td colspan="6" class="footer-title">配件表</td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
