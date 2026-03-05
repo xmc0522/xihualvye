79de617c24939a6488d50b8bcb5807e49e204a70
青衫憶笙 <16009524+xmc990408@user.noreply.gitee.com>
2026-03-05 15:23:28 +0800

    添加查询和生成按钮

--- xihualvye/src/App.vue
+++ xihualvye/src/App.vue
@@ -31,12 +31,19 @@
       />
     </el-select>
   </div>
-  
+  <div>
+    <el-button type="primary" :icon="Search">查询</el-button>
+    <el-button type="primary">
+      生成表格<el-icon class="el-icon--right"><Upload /></el-icon>
+    </el-button>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { size, value2, shortcuts } from './ts/date-picker'
 import { value1, options } from './ts/xialakuang'
+import { Search, Upload } from '@element-plus/icons-vue'//到时候执行命令安装依赖：pnpm add @element-plus/icons-vue
+
 </script>
 <style>
 @import './css/demo-date-picker.css';
