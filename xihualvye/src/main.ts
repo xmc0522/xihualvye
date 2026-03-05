7c05f55b5639a9ad0c4c989afcb8624c115d37e0
青衫憶笙 <16009524+xmc990408@user.noreply.gitee.com>
2026-03-05 11:30:40 +0800

    vant组件引入

--- xihualvye/src/main.ts
+++ xihualvye/src/main.ts
@@ -2,7 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import Vant from 'vant';
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +10,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(Vant);
 app.mount('#app')
+
